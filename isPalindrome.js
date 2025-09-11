27ebac6394c4d7aeed8c6e0d7a1bce8f30681a0c
linda-lei <131387872+linda-lei@users.noreply.github.com>
2025-09-11 01:43:59 -0700

    Create isPalindrome.js

---
+++ isPalindrome.js
@@ -0,0 +1,26 @@
+// https://www.educative.io/interview-prep/coding/valid-palindrome
+function isPalindrome(s) {
+    let left = 0, right = s.length - 1;
+
+    while (left < right) {
+        while (left < right && !/[a-zA-Z0-9]/.test(s[left])) {
+            left++;
+        }
+        
+        while (left < right && !/[a-zA-Z0-9]/.test(s[right])) {
+            right--;
+        }
+
+        if (s[left].toLowerCase() !== s[right].toLowerCase()) {
+            // write your code here
+            return false;
+        }
+        
+        left++;
+        right--;
+    }
+
+    return true;
+}
+
+export { isPalindrome };
